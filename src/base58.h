69b2dd04e3b15014122a246681373133f5c595e4
Jahvinci <ballerini.gabriel@gmail.com>
2015-09-06 13:34:40 -0700

    Name updates

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 55,
+        PUBKEY_ADDRESS = 20,
         SCRIPT_ADDRESS = 8,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
